f2a337ab86af34e3a2c170ceaf127c7b2633d188
Angelo Vera <ajb.vera@yavirac.edu.ec>
2025-10-13 09:00:13 -0500

    comandos de git

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { RegisterAuthDto } from './dto/register.auth.dto';
+import { LoginAuthDto } from './dto/login-auth.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authservice: AuthService) {}
+
+  @Post('register')
+  registerUser(@Body() userobj: RegisterAuthDto) {
+    console.log(userobj);
+    return userobj;
+  }
+
+  @Post('login')
+  Login(@Body() credenciales: LoginAuthDto) {
+    this.authservice;
+  }
+}
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { LoginAuthDto } from './dto/login-auth.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private jwtservice: JwtService){}
+  Login(credenciales: LoginAuthDto) {
+    
+  }
+}
