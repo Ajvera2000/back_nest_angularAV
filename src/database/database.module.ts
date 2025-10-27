becf8256350786e91f4f8bdbf20189d5e232635f
Hepestoo <teckalan2@gmail.com>
2025-10-27 08:15:24 -0500

    conexion_posgres

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -3,9 +3,7 @@ import { databaseProvider } from './database.providers';
 import { ConfigService } from 'src/config/config.service';
 
 @Module({
-    providers: [...databaseProvider,ConfigService],
-    exports: [...databaseProvider],
+  providers: [...databaseProvider, ConfigService],
+  exports: [...databaseProvider],
 })
-export class DatabaseModule {
-
-}
+export class DatabaseModule {}
--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,22 +1,23 @@
-import { ConfigService } from "src/config/config.service";
-import { DataSource } from "typeorm";
+import { DataSource } from 'typeorm';
+import { ConfigService } from 'src/config/config.service';
 
 export const databaseProvider = [
-    {
-        provide: 'DATABASE_CONNECTION',
-        inject: [ConfigService],
-        useFactory:(config: ConfigService)=>{
-            const dataSource = new DataSource({
-                type:'postgres',
-                host:config.get('HOST') || 'localhost',
-                port: +config.get('PORT') ,
-                username: config.get('USERNAME') || 'root',
-                password: config.get('PASSWORD')|| 'prueba',
-                database: config.get('DATABASE')
-            });
-            return dataSource.initialize();
-        }
-    },
-];
+  {
+    provide: 'DATABASE_CONNECTION',
+    inject: [ConfigService],
+    useFactory: (config: ConfigService) => {
+      const dataSource = new DataSource({
+        type: 'postgres',
+        host: config.get('HOST') || 'localhost',
+        port: +config.get('PORT') || 5432,
+        username: config.get('USERNAME') || 'postgres',
+        password: config.get('PASSWORD') || 'prueba',
+        database: config.get('DATABASE') || 'mi_base',
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true, // opcional, crea tablas automáticamente
+      });
 
- 
+      return dataSource.initialize();
+    },
+  },
+];
